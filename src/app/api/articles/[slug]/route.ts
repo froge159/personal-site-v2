7a70f701c4db1e0467f684a447bc5b663e140a74
Samuel Xie <samjh8xie@gmail.com>
2025-05-27 16:01:37 -0500

    a;ldkjf

--- src/app/api/articles/[slug]/route.ts
+++ src/app/api/articles/[slug]/route.ts
@@ -1,19 +1,22 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: Promise<{ slug: string }>}) {
-    const API_URL = process.env.NEXT_PUBLIC_BLOG_API_URL;
-    const API_KEY = process.env.API_KEY;
-    const { slug } = await params;
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  const API_URL = process.env.NEXT_PUBLIC_BLOG_API_URL;
+  const API_KEY = process.env.API_KEY;
+  const { slug } = params;
 
-    const res = await fetch(`${API_URL}/api/blogs/fetch_blog/${slug}`, {
-        headers: {
-            'X-API-Key': API_KEY || '',
-        },
-    });
+  const res = await fetch(`${API_URL}/api/blogs/fetch_blog/${slug}`, {
+    headers: {
+      'X-API-Key': API_KEY || '',
+    },
+  });
 
-    if (!res.ok) {
-        return NextResponse.json({ error: 'Failed to fetch article' }, { status: 500 });
-    }
+  if (!res.ok) {
+    return NextResponse.json({ error: 'Failed to fetch article' }, { status: 500 });
+  }
 
-    return NextResponse.json(await res.json());
+  return NextResponse.json(await res.json());
 }
