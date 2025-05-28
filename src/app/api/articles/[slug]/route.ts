738a413f97a99d0c3a0b65624d0fc5c4135960cc
Samuel Xie <samjh8xie@gmail.com>
2025-05-27 22:00:25 -0500

    as;ldkjf

--- src/app/api/articles/[slug]/route.ts
+++ src/app/api/articles/[slug]/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 
-type tParams = Promise<{ slug: string[] }>;
 
 
-export async function GET(request: Request, { params }: { params:  tParams }) {
+export async function GET(request: Request, context: { params: Promise<{ slug: string }> }) {
   const API_URL = process.env.NEXT_PUBLIC_BLOG_API_URL;
   const API_KEY = process.env.API_KEY;
-  const {slug} : {slug: string[]} = await params;
+  const params = await context.params;
+  const slug = params.slug;
 
   const res = await fetch(`${API_URL}/api/blogs/fetch_blog/${slug}`, {
     headers: {
